38bd65fac3fa44360e84985f1836be187ccec905
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2017-04-18 11:54:29 +0200

    fixed bug in PushSwitchPushCommandChain that resulted in broken navigation state

--- Example/LegacyMigrationExample/NavigationCommands/ComplexTestNavigationCommandChains.swift
+++ Example/LegacyMigrationExample/NavigationCommands/ComplexTestNavigationCommandChains.swift
@@ -9,10 +9,12 @@ struct PushSwitchPushCommandChain: NavigationCommandChain {
 		assert(titles.count >= 10)
 		// the pushes before switching tab cannot be animated - this is a UIKit bug - for more information see https://github.com/willhaben/Wireframes/issues/4
 		let pushes1: [NavigationCommand] = wfs[0..<5].map({ NavigationControllerNavigationCommand.push(wireframe: $0, animated: false) })
-		let pushes2: [NavigationCommand] = wfs[wfs.count-6..<wfs.count-1].map({ NavigationControllerNavigationCommand.push(wireframe: $0, animated: true) })
+		// cannot mix non-animated with several animated pushes on the same navigation controller, otherwise we run in a UIKit bug
+		let pushes2: [NavigationCommand] = wfs[wfs.count-6..<wfs.count-2].map({ NavigationControllerNavigationCommand.push(wireframe: $0, animated: false) })
+		let push3 = NavigationControllerNavigationCommand.push(wireframe: wfs[wfs.count-1], animated: true)
 		let switchTabCommand = TabBarControllerNavigationCommand.switchTab(toWireframeWithTag: RootTabWireframeTag.second)
 
-		let navigationCommands: [NavigationCommand] = pushes1 + [switchTabCommand] + pushes2
+		let navigationCommands: [NavigationCommand] = pushes1 + [switchTabCommand] + pushes2 + [push3]
 		return NavigationCommandSequence(navigationCommands)
 	}
 
