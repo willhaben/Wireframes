68e91e59a968b5d367719c41da5b1f09abb112e2
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2017-04-19 09:35:32 +0200

    added NS_ASSUME_NONNULL_BEGIN/END to Navigatable.h

--- Source/ViewControllers/Navigatable.h
+++ Source/ViewControllers/Navigatable.h
@@ -1,3 +1,7 @@
+NS_ASSUME_NONNULL_BEGIN
+NS_ASSUME_NONNULL_END
+
+
 /// let viewcontrollers adopt this protocol in order to get notified when its wireframe gets navigated to
 /// objective-c in order to be adoptable by objective-c viewcontrollers
 @protocol Navigatable <NSObject>
