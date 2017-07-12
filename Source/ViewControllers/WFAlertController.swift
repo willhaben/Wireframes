ed57e1b41bc08d1edc77b460bc2bd63b01a71b7f
Fabian Ehrentraud <fabian.ehrentraud@willhaben.at>
2017-07-12 15:40:36 +0200

    added missing weak modifier for wireframe property in WFAlertController

--- Source/ViewControllers/WFAlertController.swift
+++ Source/ViewControllers/WFAlertController.swift
@@ -3,7 +3,7 @@ import UIKit
 
 open class WFAlertController: UIAlertController {
 
-	public var wireframe: AlertWireframeInterface?
+	public weak var wireframe: AlertWireframeInterface?
 
 	// didMove(toParentViewController:) is not called for UIAlertController instances, whyever - so we have to use viewDidDisappear instead, which should work ok, as nothing should be presented on top
 	override open func viewDidDisappear(_ animated: Bool) {
